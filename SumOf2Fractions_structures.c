054dc769c62b25c4eac1d05f25e13cc84af250b2
VaishnaviPunagin <48246776+VaishnaviPunagin@users.noreply.github.com>
2019-03-07 11:31:54 -0800

    Create SumOf2Fractions_structures.c

---
+++ SumOf2Fractions_structures.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+struct fraction
+{
+    int num,den;
+}fraction;
+
+void in(int *num,int *den)
+{
+    printf("\nEnter value of numerator    :   ");
+    scanf("%d",num);
+    printf("\nEnter value of denominator  :   ");
+    scanf("%d \n",den);
+}
+
+struct fraction add(struct fraction f1,struct fraction f2)
+{
+    struct fraction sum={((f1.num*f2.den)+(f2.num*f1.den)),(f1.den*f2.den)};
+    return sum;
+}
+
+void output(struct fraction sum)
+{
+    printf("SUM IS %d / %d .",sum.num,sum.den);
+    
+}
+
+int main()
+{
+    int num1,den1,num2,den2;
+    in(&num1,&den1);
+    in(&num2,&den2);
+    struct fraction f1={num1,den1};
+    struct fraction f2={num2,den2};
+    struct fraction result=add(f1,f2);
+    output(result);
+    return 0;
+    
+}
