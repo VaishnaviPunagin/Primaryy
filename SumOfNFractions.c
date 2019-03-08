08cadd3f629f836096359c00a33c585c1a37242c
VaishnaviPunagin <48246776+VaishnaviPunagin@users.noreply.github.com>
2019-03-08 10:28:30 -0800

    Create SumOfNFractions.c

---
+++ SumOfNFractions.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+
+struct fraction
+{
+    int num,den;
+}f1,f2;
+
+void inputn(int *n)
+{
+      printf("How many fractions do you want to add ?\n");
+      scanf("%d",n);
+}
+
+void input(int n,struct fraction f[])
+{
+      for(int i=0;i<n;i++)
+      {
+            printf("\nEnter value of numerator %d : ",i+1);
+            scanf("%d",&f[i].num);
+            printf("\nEnter value of denominator %d : ",i+1);
+            scanf("%d",&f[i].den);
+      }
+}
+
+struct fraction add(int n,struct fraction f[])
+{
+      int i,num=0,den=1;
+      for(i=0;i<n;i++)
+            den=den*f[i].den;
+      for(i=0;i<n;i++)
+            num=num+((f[i].num*den)/f[i].den);
+      struct fraction sum={num,den};
+      return sum;
+}
+
+void output(struct fraction sum)
+{
+        printf("SUM IS %d / %d .",sum.num,sum.den);
+}
+
+int main()
+{
+      int n;
+      inputn(&n);
+      struct fraction f[n];
+      input(n,f);
+      struct fraction result=add(n,f);
+      output(result);
+      return 0;
+}
