b53b5d5f31eda37e702dd2ce24f8c7cd55fb5158
VaishnaviPunagin <48246776+VaishnaviPunagin@users.noreply.github.com>
2019-03-12 16:59:32 +0530

    Update SumOf2Fractions_structures.c

--- SumOf2Fractions_structures.c
+++ SumOf2Fractions_structures.c
@@ -1,10 +1,9 @@
-
 #include<stdio.h>
 
 struct fraction
 {
     int num,den;
-}f1,f2;
+};
 
 struct fraction input()
 {
@@ -22,15 +21,15 @@ struct fraction add(struct fraction f1,struct fraction f2)
     return sum;
 }
 
-struct fraction output(struct fraction sum)
+void output(struct fraction sum)
 {
     printf("SUM IS %d / %d .",sum.num,sum.den);
 }
 
 int main()
 {
-    f1=input();
-    f2=input();
+    struct fraction f1=input();
+    struct fraction f2=input();
     struct fraction result=add(f1,f2);
     output(result);
     return 0;
