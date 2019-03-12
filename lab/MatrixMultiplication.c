d650afa680dbb32ea53328a945c54f2d2468b82a
VaishnaviPunagin <48246776+VaishnaviPunagin@users.noreply.github.com>
2019-03-12 17:43:24 +0530

    Update MatrixMultiplication.c

--- lab/MatrixMultiplication.c
+++ lab/MatrixMultiplication.c
@@ -11,7 +11,7 @@ void inputmatrix(int m,int n,int matrix[m][n])
 	for(int i=0;i<m;i++)
 		for(int j=0;j<n;j++)
 		{
-			printf("Enter value into matrix for position [%d][%d]",i,j);
+			printf("Enter value into matrix for position [%d][%d] ",i,j);
 			scanf("%d",&matrix[i][j]);
 		}
 }
