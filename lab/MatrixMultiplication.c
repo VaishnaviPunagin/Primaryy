76f66b3392f49e6af34585cdd58b5cd1e58513ab
VaishnaviPunagin <48246776+VaishnaviPunagin@users.noreply.github.com>
2019-03-13 18:47:27 +0530

    Update MatrixMultiplication.c

--- lab/MatrixMultiplication.c
+++ lab/MatrixMultiplication.c
@@ -16,22 +16,33 @@ void inputmatrix(int m,int n,int matrix[m][n])
 		}
 }
 
-void multiply(int m,int n,int m1[m][n],int p,int q,int m2[p][q],int m3[m][q])
+int multiply(int m,int n,int m1[m][n],int p,int q,int m2[p][q],int m3[m][q])
 {
-	int i,j,k;
-	for(i=0;i<m;i++)
-		for(j=0;j<q;j++)
-			m3[i][j]=0;
+	int i,j,k,f;
+	if(n==p)
+	{
+		for(i=0;i<m;i++)
+			for(j=0;j<q;j++)
+				m3[i][j]=0;
 	
-	for(i=0;i<m;i++)
-		for(j=0;j<q;j++)
-			for(k=0;k<n;k++)
-				m3[i][j]+=m1[i][k]*m2[k][j];
+		for(i=0;i<m;i++)
+			for(j=0;j<q;j++)
+				for(k=0;k<n;k++)
+					m3[i][j]+=m1[i][k]*m2[k][j];
+		f=0;
+		return f;
+	}
+	
+	else
+	{
+		f=1;
+		return f;
+	}
 }
 
-void output(int m,int n,int p,int q,int m3[m][q])
+void output(int f,int m,int q,int m3[m][q])
 {
-	if(n==q)
+	if(f==0)
 	{	
 		printf("The result of multiplication of the entered matrix is : \n");	
 		for(int i=0;i<m;i++)
@@ -47,7 +58,7 @@ void output(int m,int n,int p,int q,int m3[m][q])
 
 int main()
 {
-	int m,n,p,q;
+	int m,n,p,q,f;
 	inputorder(&m,&n);
 	int m1[m][n];
 	inputmatrix(m,n,m1);
@@ -55,7 +66,7 @@ int main()
 	int m2[p][q];
 	inputmatrix(p,q,m2);
 	int m3[m][q];
-	multiply(m,n,m1,p,q,m2,m3);
-	output(m,n,p,q,m3);
+	f=multiply(m,n,m1,p,q,m2,m3);
+	output(f,m,q,m3);
 	return 0;
 }
