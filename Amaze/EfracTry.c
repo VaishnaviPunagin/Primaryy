7243dc0d05ae7f2f49e3ce6e20b2180b65cb2947
VaishnaviPunagin <48246776+VaishnaviPunagin@users.noreply.github.com>
2019-03-14 18:08:43 +0530

    Create EfracTry.c

---
+++ Amaze/EfracTry.c
@@ -0,0 +1,39 @@
+//EFRAC TRIAL LOL
+#include<stdio.h>
+#include<math.h>
+
+struct fraction
+{
+	int num,den;
+};
+
+struct fraction input()
+{
+	struct fraction f;
+	printf("Enter numerator and denominator of the fraction : \n");
+	scanf("%d %d",&f.num,&f.den);
+	return f;
+}
+
+struct fraction subtract(struct fraction f, struct fraction f1)
+{
+	struct fraction f2;
+	f2.den=(f.den*f1.den);
+	f2.num=(f.num*f1.den-f1.num*f.den);
+	return f2;
+}
+
+cal(struct fraction *f[1000])
+{
+	double d =ceil( ((double)f[0].den/(double)f[0].num));
+	*f[1]={1,d};
+	*f1={1,d};
+
+}
+
+int main()
+{
+	struct fraction f[1000];
+	f[0]=input();
+	
+}
