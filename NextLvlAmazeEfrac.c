f4fdb613e0a66878843040123dd2f761dc3e9ec9
VaishnaviPunagin <48246776+VaishnaviPunagin@users.noreply.github.com>
2019-03-14 12:32:58 +0530

    Create NextLvlAmazeEfrac.c

---
+++ NextLvlAmazeEfrac.c
@@ -0,0 +1,43 @@
+//EFRAC TRIAL LOL
+#include<stdio.h>
+#include<math.h>
+
+struct fraction
+{
+	int num,den;
+};
+
+struct fraction input()
+{
+	struct fraction f;
+	printf("Enter numerator and denominator of the fraction : \n");
+	scanf("%d %d",&f.num,&f.den);
+	return f;
+}
+
+struct fraction subtract(struct fraction f, struct fraction f1)
+{
+	struct fraction f2;
+	f2.den=(f.den*f1.den);
+	f2.num=(f.num*f1.den-f1.num*f.den);
+	return f2;
+}
+
+struct fraction cal(struct fraction f)
+{
+	double a = (double) (f.den/f.num);
+	int d= ceil(a);
+	struct fraction f1={1,d};
+	struct fraction f2=subtract(f,f1);
+	printf("%d/%d=%d/%d%d/%d",f.num,f.den,f1.num,f1.den,f2.num,f2.den);
+	struct fraction q={0,0};
+	return q;
+
+}
+
+int main()
+{
+	struct fraction f=input();
+	struct fraction q=cal(f);
+	return 0;
+}
