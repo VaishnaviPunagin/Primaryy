8a5289beef9e25925e435b7ad64850ddca263720
VaishnaviPunagin <48246776+VaishnaviPunagin@users.noreply.github.com>
2019-03-26 16:11:18 +0530

    Update IntersectingCircles.c

--- 20-20/IntersectingCircles.c
+++ 20-20/IntersectingCircles.c
@@ -1,44 +1,57 @@
-#include <stdio.h>
+#include<stdio.h>
 #include<math.h>
+
+void testcases(int *t)
+{
+	printf("How many pairs of circles? ");
+	scanf("%d",t);
+}
 
 struct center
 {
-    float x,y;
+	float x,y;
 };
-
 
 struct circles
 {
-    float radius;
-    struct center c;
-    
+	float r;
+	struct center c;
 };
 
-void howmanypairsofcircles(int *n)
+void getcircles(int t,struct circles circle[t])
 {
-    printf("How many pairs of circes do you want to work with? ");
-    scanf("%d",n);
-    
+	for(int i=0;i<t;i++)
+	{
+		printf("Radius of circle %d. : ",i+1);
+		scanf("%f",&circle[i].r);
+		printf("Co-ordinates of center of circle %d.1 : ",i+1);
+		scanf("%f %f",&circle[i].c.x,&circle[i].c.y);
+	}
 }
 
-
-void getvalues(int n,struct circles circle[2n])
+void check(int t,struct circles circle[t])
 {
-    for(int i=0;i<n;i++)
-    {
-        printf("Enter radius and center coordinates(x,y) for circle 1 of pair %d : ",i+1);
-        scanf("%f %f %f",&circle[i].radius,&circle[i].c.x,&circle[i].c.y);
-        
-    }
+	for(int i=0;i<t;i+=2)
+	{
+		float dc=sqrt(pow(circle[i].c.x-circle[i+1].c.x,2)+pow(circle[i].c.y-circle[i+1].c.y,2));
+		float d=circle[i].r+circle[i+1].r;
+		if(dc>d)
+			printf("TWO intersect \n");
+		if(dc==d)
+			printf("Intersect one point \n");
+		if(dc<d)
+			printf("NO POINTS intersect \n");
+	}
 }
 
 int main()
 {
-    int n;
-    howmanypairsofcircles(&n);
-    struct circles circle[2n];
-    struct center c[2n];
-    getvalues(n,circle);
-    
+	int t;
+	testcases(&t);
+	t=2*t;
+	struct circles circle[t];
+	struct center c[t];
+	getcircles(t,circle);
+	check(t,circle);
+	return 0;
 }
-
