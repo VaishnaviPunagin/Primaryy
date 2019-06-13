181b2acfc57fbfc3fe8b0b63b1b5adeb6c9222f4
VaishnaviPunagin <48246776+VaishnaviPunagin@users.noreply.github.com>
2019-06-13 21:34:53 +0530

    Update 2DcharArray.c

--- DataStructures/2DcharArray.c
+++ DataStructures/2DcharArray.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
 int main()
 {
-	char s[10];
-	printf("Enter word : ");
-	scanf("%s",s);
-	printf("Stored string is :  %s\n",s);
+	char s[3][10];
+	for(int i=0;i<3;i++)
+	{	
+		printf("Enter word : ");
+		scanf("%s",s[i]);
+	}
+	printf("String")
+	for(int i=0;i<3;i++)
+		printf("Stored string is :  %s\n",s[i]);
 	return 0;
 }
