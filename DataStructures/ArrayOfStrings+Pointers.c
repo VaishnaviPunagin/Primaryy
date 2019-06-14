3627597ab71f6b8a49a14333b44bb50953b7bb5b
VaishnaviPunagin <48246776+VaishnaviPunagin@users.noreply.github.com>
2019-06-14 10:44:09 +0530

    Update ArrayOfStrings+Pointers.c

--- DataStructures/ArrayOfStrings+Pointers.c
+++ DataStructures/ArrayOfStrings+Pointers.c
@@ -1,6 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+void input(int n,char *s[n])
+{
+	int size=100;
+	for(int i=0;i<n;i++)
+	{
+		s[i]=malloc(100);
+		printf("Enter word : ");
+		scanf("%s",s[i]);
+	}
+}
 
 int main()
 {
@@ -8,13 +18,7 @@ int main()
 	printf("How many strings ??");
 	scanf("%d",&n);	
 	char *s[n];
-	int size=1000;
-	for(int i=0;i<n;i++)
-	{
-		s[i]=malloc(size*sizeof(char));
-		printf("Enter word : ");
-		scanf("%s",s[i]);
-	}
+	input(n,s);
 	printf("WORDS STORED ARE : \n");
 	for(int i=0;i<n;i++)	
 		printf("%s  ",s[i]);	
