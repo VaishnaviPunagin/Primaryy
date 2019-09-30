43a5cd5046933fd1898a8cde62834b2a9c03525d
VaishnaviPunagin <48246776+VaishnaviPunagin@users.noreply.github.com>
2019-09-30 11:52:30 +0530

    Update MainMenu.c

--- Project/MainMenu.c
+++ Project/MainMenu.c
@@ -3,24 +3,30 @@
 
 //function declarations KEEP TRACK PLIS
 
-void newuser();
-void existinguser();
-void admin();
+//void customer();
+//void admin();
+ 
+
 
 int MainMenu()
 {
 	printf("*********************\nWELCOME TO THE HOTEL\n*************************\n");
-	printf("1)New User\n2)Existing User3)Admin Log in\n4)Exit\nPlease pick an option :: ");
+	printf("1)Customer Log in\n2)Admin Log in\n3)Exit\nPlease pick an option :: ");
 	int option;
 	scanf("%d",&option);
 	switch(option)
 	{
-		case 1 : newuser(); break;
-		case 2 : existinguser(); break;
-		case 3 : admin(); break;
-		case 4 : exit(0);
+		//case 1 : customer(); break;
+		//case 2 : admin(); break;
+		case 3 : exit(0);
 		default : printf("ERROR. GO DIE\n\n");
 	}
+	MainMenu();
+	return 0;
+}
+
+int main()
+{
 	MainMenu();
 	return 0;
 }
