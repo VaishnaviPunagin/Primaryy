0b8a6261e5c908359f0fe808e75779019396f41d
VaishnaviPunagin <48246776+VaishnaviPunagin@users.noreply.github.com>
2019-12-08 23:49:28 +0530

    Update ActualProjectFile.c

--- Project/ActualProjectFile.c
+++ Project/ActualProjectFile.c
@@ -1,3 +1,4 @@
+
 #include<time.h>
 #include<stdio.h>
 #include<stdlib.h>
@@ -120,27 +121,27 @@ void header()
 {
 	system("clear");
 	printf("\n");
-  printf("||-------------------------------------------------------------------------------------------------||\n\n");
-  printf("||-------------------------------------------------------------------------------------------------||\n\n");
+  printf("||---------------------------------------------------------------------------------------------------||\n");
+  printf("||---------------------------------------------------------------------------------------------------||\n");
   printf("|| 		 		H    H    OOOOO   TTTTTTT  EEEEEE  L                		     ||\n");
   printf("||  		 		H    H   O     O     T     E       L                		     ||\n");
   printf("|| 		 		HHHHHH  O   O   O    T     EEEE    L                		     ||\n");
   printf("||  		 		H    H   O     O     T     E       L                		     ||\n");
   printf("||  		 		H    H    OOOOO      T     EEEEEE  LLLLLL           		     ||\n");
-  printf("\n");
-  printf("||	MM   MM      A      NN    N      A      GGGGG  EEEEEE  MM   MM  EEEEEE  NN    N  TTTTTTT     ||\n");
-  printf("||    M M M M     A A     N N   N     A A     G      E       M M M M  E       N N   N     T        ||\n");
-  printf("||    M  M  M    A   A    N  N  N    A   A    G  GG  EEEE    M  M  M  EEEE    N  N  N     T        ||\n");
-  printf("||    M     M   A A A A   N   N N   A A A A   G   G  E       M     M  E       N   N N     T        ||\n");
-  printf("||    M     M  A       A  N    NN  A       A  GGGGG  EEEEEE  M     M  EEEEEE  N    NN     T        ||\n");
-  printf("\n");
+  printf("||                                                                                                   ||\n");
+  printf("||    MM   MM      A      NN    N      A      GGGGG  EEEEEE  MM   MM  EEEEEE  NN    N  TTTTTTT       ||\n");
+  printf("||    M M M M     A A     N N   N     A A     G      E       M M M M  E       N N   N     T          ||\n");
+  printf("||    M  M  M    A   A    N  N  N    A   A    G  GG  EEEE    M  M  M  EEEE    N  N  N     T          ||\n");
+  printf("||    M     M   A A A A   N   N N   A A A A   G   G  E       M     M  E       N   N N     T          ||\n");
+  printf("||    M     M  A       A  N    NN  A       A  GGGGG  EEEEEE  M     M  EEEEEE  N    NN     T          ||\n");
+  printf("||                                                                                                   ||\n");
   printf("||  				SSSSSS  Y   Y  SSSSSS  TTTTTTT  EEEEEE  MM   MM 		     ||\n");
   printf("||  				S        Y Y   S          T     E       M M M M    		     ||\n");
   printf("||  				SSSSSS    Y    SSSSSS     T     EEEE    M  M  M    		     ||\n");
   printf("||  				     S    Y         S     T     E       M     M    		     ||\n");
   printf("||  				SSSSSS    Y    SSSSSS     T     EEEEEE  M     M    		     ||\n");
-  printf("||-------------------------------------------------------------------------------------------------||\n");
-  printf("||-------------------------------------------------------------------------------------------------||\n\n");
+  printf("||---------------------------------------------------------------------------------------------------||\n");
+  printf("||---------------------------------------------------------------------------------------------------||\n\n");
 
 }
 
