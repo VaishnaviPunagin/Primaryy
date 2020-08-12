0cad5652b2547d5642272a5fbb1f9bce5304dd9b
VaishnaviPunagin <48246776+VaishnaviPunagin@users.noreply.github.com>
2020-08-12 09:43:17 +0530

    Create Employee.cpp

---
+++ CPP/Employee.cpp
@@ -0,0 +1,49 @@
+/*2.  Write a program by creating an 'Employee' class having the following functions and print the final salary.
+1 - 'getInfo()' which takes the salary, number of hours of work per day of employee as parameters
+2 - 'AddSal()' which adds $10 to the salary of the employee if it is less than $500.
+3 - 'AddWork()' which adds $5 to the salary of the employee if the number of hours of work per day is more than 6 hours.*/
+
+#include<iostream>
+using namepsace std;
+
+class Employee
+{
+	int Salary, HoursPerDay;
+	int AddSal(Salary), AddWork(Salary),Changes();	
+	public: void getinfo();
+};
+
+void Employee::AddSalary(int salary)
+{
+	salary+=10;
+	return salary;
+}
+
+void Employee::AddWork(int salary)
+{
+	salary+=5;
+	return salary;
+}
+
+void Employee::Changes()
+{
+	if(Salary<=500)
+		Salary=AddSal(Salary);
+	if(HoursPerDay>=6)
+		Salary=AddWork(Salary);
+}
+
+void Employee::getinfo()
+{
+	cin>>Salary>>HoursPerDay;
+	cout<<"Entered Salary : "<<Salary<<" and Hours of wokr per day : "<<HoursPerDay<<" ."<<endl;
+	Changes();
+	cout<<"After certain evaluation, updated Salary is "<<Salary<<" ! Thank you."<<endl;
+}
+
+int main()
+{
+	class Employee first;
+	first.getinfo();
+	return 0;
+}
