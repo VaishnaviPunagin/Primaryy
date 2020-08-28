44de49946c3d121fe4ea0f85568d3a921c394f2e
VaishnaviPunagin <48246776+VaishnaviPunagin@users.noreply.github.com>
2020-08-28 17:06:44 +0530

    Create 9.CallByReference.cpp

---
+++ CPP/9.CallByReference.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+class content{
+  int testNumber;
+public:
+  content(int number){
+    testNumber=number;
+  }
+  void change(content &test){
+    test.testNumber+=testNumber;
+    test.testNumber+=500;
+  }
+  void display(){
+    cout<<"Content stored : "<<testNumber<<"\n";
+  }
+};
+
+int main()
+{
+  content first(10),second(230);
+  cout<<"Before any change : \n"<<"First object : ";
+  first.display();
+  cout<<"Second object : ";
+  second.display();
+  first.change(second);
+  cout<<"\nAfter calling the change() function : \n"<<"First object : ";
+  first.display();
+  cout<<"Second object : ";
+  second.display();
+  cout<<"^^Change function calls the second object by reference and adds content of object first and 500 to the content of the second object!";
+  return 0;
+}
